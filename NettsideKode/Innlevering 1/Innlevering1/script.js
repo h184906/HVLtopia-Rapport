879175cef462ba743ae636607ff89f5755600ae7
h184906 <184906@stud.hvl.no>
2024-10-08 22:13:41 +0200

    Løsning resultat Ferdig
    Fikset en "ok" tekst samt fikest bildet og en finere bakgrunn til skrift teksten, og gjort variabel navn i js litt lettere å forstå, samt bytte ene til norsk for mer like variabler i koden.

--- NettsideKode/Innlevering 1/Innlevering1/script.js
+++ NettsideKode/Innlevering 1/Innlevering1/script.js
@@ -1,14 +1,14 @@
 const løsningDiv = document.getElementById("løsning_div");
 const resultatDiv = document.getElementById("resultat_div");
-const resultBox = document.getElementById("løsning_resultat_utskrift_box");
+const resultat_box = document.getElementById("løsning_resultat_utskrift_box");
 
 løsningDiv.addEventListener("click", function() {
-    resultBox.innerHTML = "";
-    resultBox.innerHTML = "<h2>Løsning</h2> <p>Løsningen vår er...</p>";
+    resultat_box.innerHTML = "";
+    resultat_box.innerHTML = "<h2>Løsning</h2> <h3>Transport</h3><p>Nye transport muligheter som hyperloopen, dronereisen og undervannstunellen</p><h3>Energibruk</h3><p>Nye måter å bruke å få energi har gjort bergen mer miljøvennlig og mer bærekraftig</p>";
 });
 
 resultatDiv.addEventListener("click", function() {
     console.log("hei?")
-    resultBox.innerHTML = "";
-    resultBox.innerHTML = "<h2>Resulat</h2><p>Resultatet vårt er....</p>";
+    resultat_box.innerHTML = "";
+    resultat_box.innerHTML = "<h2>Resulat</h2><h3>Mer bærekraftig</h3><p>Med en bedre fokus på energi bruk og produksjonen har Bergen blitt mer bærekraftig</p><h3>Effektivitet og inovasjon</h3><p>Med de nye dronereisene, hyperloopen og undervannstunellene har det blitt raskere, mer effektivt og bedre for miljøet å reise i sentrum</p>";
 });
